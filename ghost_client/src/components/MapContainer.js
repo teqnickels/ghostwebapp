c511a54371a5b3aba13fc1fe5b040344117f8d49
TeqNickels <debrenamcewen@gmail.com>
2018-02-28 16:09:54 -0800

    Removes hard coded marker, adds marker seed object

--- ghost_client/src/components/MapContainer.js
+++ ghost_client/src/components/MapContainer.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
-// import Map from './Map';
+import markers from '../data/markersData';
 import { GOOGLE_MAPS_API_KEY } from '../config';
 
 export class MapContainer extends Component {
+
+  renderMarkers = () => {
+    return markers.map(marker => 
+      <Marker 
+        key={marker.title}
+        title={marker.title} 
+        position={{lat: marker.lat, lng: marker.lng}} 
+        name={marker.name} 
+      />
+    );
+  };
+
   render() {
     if (!this.props.loaded) {
       return <div>Loading...</div>
     }
-
+    
     return (
       <div>      
         <Map google={this.props.google}
             style={{width: '100%', height: '100%', position: 'relative'}}
             className={'map'}
             zoom={14}>
-          <Marker
-            title={'The marker`s title will appear as a tooltip.'}
-            name={'SOMA'}
-            position={{lat: 37.778519, lng: -122.405640}} />
-          <Marker
-            name={'Dolores park'}
-            position={{lat: 37.759703, lng: -122.428093}} />
-          <Marker />
+
+          {this.renderMarkers()}
         </Map>
       </div>
     );
